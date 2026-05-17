5fee6401b2c07c67bc34849eb40ba2f0f1664b1c
Jeremy <jeremy@chaufourier.fr>
2026-05-17 19:01:17 +0200

    chore(deps): add sharp and tesseract.js

--- config/redis.ts
+++ config/redis.ts
@@ -1,6 +1,7 @@
+import type { InferConnections } from '@adonisjs/redis/types'
+
 import env from '#start/env'
 import { defineConfig } from '@adonisjs/redis'
-import { InferConnections } from '@adonisjs/redis/types'
 
 const redisConfig = defineConfig({
   connection: 'main',
