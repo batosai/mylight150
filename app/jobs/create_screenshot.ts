0d434f56cb8eb935f18e564b7463ca47b5849c31
Jeremy <jeremy@chaufourier.fr>
2026-05-17 19:33:36 +0200

    feat(jobs): implement create_screenshot job using adb

--- app/jobs/create_screenshot.ts
+++ app/jobs/create_screenshot.ts
@@ -1,9 +1,16 @@
-import { Job } from '@adonisjs/queue'
 import type { JobOptions } from '@adonisjs/queue/types'
+import type AdbService from '#services/adb_service'
+
+import app from '@adonisjs/core/services/app'
+import { Job } from '@adonisjs/queue'
+import OCRScreenshot from '#jobs/ocr_screenshot'
 
 interface CreateScreenshotPayload {
-  // Define your payload type here
+  serial?: string
 }
+
+const APP_PACKAGE_NAME = process.env.ADB_APP_PACKAGE_NAME ?? 'com.example.app'
+const SCREENSHOT_PATH = app.tmpPath('screen.png')
 
 export default class CreateScreenshot extends Job<CreateScreenshotPayload> {
   static options: JobOptions = {
@@ -11,9 +18,19 @@ export default class CreateScreenshot extends Job<CreateScreenshotPayload> {
     maxRetries: 3,
   }
 
+  constructor(private adb: AdbService) {
+    super()
+  }
+
   async execute() {
-    // Your job logic here
-    console.log('Processing CreateScreenshot', this.payload)
+    const { serial } = this.payload
+
+    await this.adb.wake({ serial })
+    await this.adb.unlock({ serial })
+    await this.adb.launchApp(APP_PACKAGE_NAME, { serial })
+    await this.adb.screencap(SCREENSHOT_PATH, { serial })
+
+    await OCRScreenshot.dispatch({})
   }
 
   async failed(error: Error) {
